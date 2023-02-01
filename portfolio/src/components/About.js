44e071dceb62e8b408995f25f7f205efc77498cd
derek helgoe <derek1896@gmail.com>
2023-01-31 21:09:06 -0600

    updates

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -5,6 +5,7 @@ import "../styles/About.css";
 export default function About () {
  return (
     <div>
+        <h1>About Me!</h1>
         <p class="words">
         My name is Derek Helgoe. I'm a current student in the full stack development program through the University of Minnesota. I reside in the twin cities. Outside of coding and learning I like to enjoy the warm weather outdoors, sports and video games with friends. This application is still in early development as more design, links/photos will be added before the final product is finished!
         {/* <img src={myimage}></img> */}
